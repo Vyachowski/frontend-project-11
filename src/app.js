d1ab1992f660b6e8380ebe888bdb7998c8e5dbda
Slava Haikin <97975418+Vyachowski@users.noreply.github.com>
2024-01-12 16:47:38 +0200

    feat: add logic for RSS adding form

--- src/app.js
+++ src/app.js
@@ -1,5 +1,67 @@
-const app = () => {
+import {
+  object, string,
+} from 'yup';
+import onChange from 'on-change';
 
+const app = () => {
+  // Rss-form component
+  const urlSchema = object({ url: string().url().nullable() });
+
+  const rssFormElement = document.querySelector('.rss-form');
+  const [rssFormInputElement, rssFormButtonElement] = rssFormElement.elements;
+
+  const state = {
+    rssUrls: [],
+    state: null, // error, filling, sending, sent
+    rssForm: {
+      url: null,
+    },
+    errors: {},
+  };
+
+  const watchedState = onChange(state, (path, value) => {
+    const render = {
+      error: () => {
+        rssFormInputElement.classList.add('is-invalid');
+        rssFormButtonElement.disabled = true;
+
+      },
+      filling: () => {
+        rssFormInputElement.classList.remove('is-invalid');
+        rssFormButtonElement.disabled = false;
+      },
+      sending: () => {
+        rssFormButtonElement.disabled = true;
+      },
+      sent: () => {
+        rssFormInputElement.value = '';
+      },
+    };
+
+    if (path === 'state') {
+      render[value]();
+    }
+  });
+
+  rssFormInputElement.addEventListener('input', (e) => {
+    watchedState.rssForm.url = e.target.value;
+    urlSchema.validate(watchedState.rssForm)
+      .then((r) => {
+        const lastIndex = watchedState.rssUrls.length;
+        watchedState.rssUrls[lastIndex] = r.url;
+        watchedState.state = 'filling';
+      })
+      .catch((err) => {
+        watchedState.state = 'error';
+        watchedState.errors = err;
+      });
+  });
+
+  rssFormButtonElement.addEventListener('click', (e) => {
+    e.preventDefault();
+    watchedState.state = 'sending';
+    watchedState.state = 'sent';
+  });
 };
 
 export default app;
