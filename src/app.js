e301d7aede71280884466b6e206f2a2b45a5da98
Slava Haikin <97975418+Vyachowski@users.noreply.github.com>
2024-01-14 10:49:08 +0200

    refactor: separate controllers from main app file

--- src/app.js
+++ src/app.js
@@ -1,46 +1,9 @@
-import axios from 'axios';
-import { rssFormButtonElement, rssFormInputElement, watchedState } from './rssView.js';
-import createUrlSchema from './utilities/urlSchema.js';
+import { rssFormElement, rssFormInputElement } from './rssView.js';
+import { inputController, formController } from './rssControllers.js';
 
 const app = (i18next) => {
-  // Load RSS Form component
-  const urlSchema = createUrlSchema();
-
-  rssFormInputElement.addEventListener('input', (e) => {
-    watchedState.rssForm.url = e.target.value;
-
-    urlSchema.validate(watchedState.rssForm)
-      .then((r) => {
-        watchedState.rssUrl = r.url;
-        watchedState.errors = [];
-        watchedState.state = 'filling';
-      })
-      .catch((err) => {
-        const errorMessageKey = `rssForm.${err.errors}`;
-        watchedState.errors = i18next.t(errorMessageKey);
-        watchedState.state = 'error';
-      });
-  });
-
-  rssFormButtonElement.addEventListener('click', (e) => {
-    e.preventDefault();
-    watchedState.state = 'sending';
-
-    axios.get('https://allorigins.hexlet.app/get?disableCache=true&url=https://lorem-rss.hexlet.app/feed')
-      .then(({ data }) => {
-        if (data) {
-          return new window.DOMParser().parseFromString(data.contents, 'text/xml');
-        }
-        throw Error('Something went wrong. Please, try again');
-      })
-      .then((rss) => console.log(rss))
-      .then(() => {
-        watchedState.state = 'sent';
-      })
-      .catch((err) => {
-        watchedState.errors = err.message;
-      });
-  });
+  rssFormInputElement.addEventListener('input', (e) => inputController(e, i18next));
+  rssFormElement.addEventListener('submit', (e) => formController(e));
 };
 
 export default app;
