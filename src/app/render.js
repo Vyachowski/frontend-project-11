c939102bfc7d0a7200728219792d2cb4bcdaae4d
Slava Haikin <97975418+Vyachowski@users.noreply.github.com>
2024-01-14 21:24:15 +0200

    feat: add rss feed list render

--- src/app/render.js
+++ src/app/render.js
@@ -1,9 +1,31 @@
+import uniqueId from 'lodash.uniqueid';
 import setElementText from '../utilities/setElementText.js';
 import setElementStyle from '../utilities/setElementStyle.js';
+import {
+  createFeedListButtonElement,
+  createFeedItemElement,
+  createFeedListLinkElement,
+} from '../utilities/createElements.js';
 
 const messageElement = document.querySelector('.feedback');
 const formElement = document.querySelector('.rss-form');
 const [inputElement, buttonElement] = formElement.elements;
+
+const renderFeedList = (parentElement, posts) => {
+  const itemsList = posts.map((post) => {
+    const postUniqueId = uniqueId();
+    const itemElement = createFeedItemElement();
+    const linkElement = createFeedListLinkElement(post.link, postUniqueId);
+    const listButtonElement = createFeedListButtonElement(postUniqueId);
+
+    itemElement.appendChild(linkElement);
+    itemElement.appendChild(listButtonElement);
+
+    return itemElement;
+  });
+
+  itemsList.forEach((item) => parentElement.appendChild(item));
+};
 
 const renderError = (errorText) => {
   setElementText(messageElement, errorText);
@@ -22,10 +44,13 @@ const renderSending = () => {
   buttonElement.disabled = true;
 };
 
-const renderSent = () => {
+const renderSent = (posts, title, description) => {
+  console.log(title, description);
+  const listGroup = document.querySelector('.list-group');
   setElementStyle(messageElement, 'success');
-  setElementText(messageElement, 'RSS успешно загружен') // TODO Change to dynamic message
+  setElementText(messageElement, 'RSS успешно загружен'); // TODO Change to dynamic message
   setElementText(inputElement, '');
+  renderFeedList(listGroup, posts);
 };
 
 const render = (state, error) => {
