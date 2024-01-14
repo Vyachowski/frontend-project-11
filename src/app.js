51383448bd51b10261685012edf548a44002439e
Slava Haikin <97975418+Vyachowski@users.noreply.github.com>
2024-01-14 11:27:49 +0200

    refactor: rename imports

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
-import { rssFormElement, rssFormInputElement } from './rssView.js';
-import { inputController, formController } from './rssController.js';
+import { rssFormElement, rssFormInputElement } from './appView.js';
+import { inputController, formController } from './appController.js';
 
 const app = (i18next) => {
   rssFormInputElement.addEventListener('input', (e) => inputController(e, i18next));
