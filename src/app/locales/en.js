752c217391804b52ffa3f2700d8a2efb6f3ffbbc
Slava Haikin <97975418+Vyachowski@users.noreply.github.com>
2024-01-15 20:14:14 +0200

    feat: add new translation files structure

--- src/app/locales/en.js
+++ src/app/locales/en.js
@@ -1,24 +1,28 @@
 export default {
   translation: {
-    rssForm: {
-      error: 'Error: ',
-      networkError: 'No response from the server. Check network connection.',
-      title: 'RSS aggregator',
-      subtitle: 'Start read RSS today! It is easy, it is fancy.',
-      inputText: 'RSS link',
-      buttonText: 'Add new',
-      descriptionText: 'Example : https://lorem-rss.hexlet.app/feed',
-      errorMessages: '',
-      urlInvalid: 'Invalid URL format',
-      urlRequired: 'Url should be not empty',
-      xmlError: 'Invalid response format: not a XML/RSS',
-    },
-    post: {
-      title: 'Posts',
-      button: 'Show',
-    },
-    feed: {
-      title: 'Feeds',
+    interfaceText: {
+      rssForm: {
+        title: 'RSS aggregator',
+        subtitle: 'Start read RSS today! It is easy, it is fancy.',
+        inputText: 'RSS link',
+        buttonText: 'Add new',
+        descriptionText: 'Example : https://lorem-rss.hexlet.app/feed',
+      },
+      post: {
+        title: 'Posts',
+        button: 'Show',
+      },
+      feed: {
+        title: 'Feeds',
+      },
+      errors: {
+        error: 'Error: ',
+        networkError: 'No response from the server. Check network connection.',
+        errorMessages: '',
+        urlInvalid: 'Invalid URL format',
+        urlRequired: 'Url should be not empty',
+        xmlError: 'Invalid response format: not a XML/RSS',
+      },
     },
   },
 };
--- src/app/locales/ru.js
+++ src/app/locales/ru.js
@@ -1,24 +1,28 @@
 export default {
   translation: {
-    rssForm: {
-      error: 'Ошибка: ',
-      networkError: 'Отсутствует ответ от сервера. Возможно, нет сети.',
-      title: 'RSS агрегатор',
-      subtitle: 'Начните читать RSS сегодня! Это легко, это красиво.',
-      inputText: 'Ссылка RSS',
-      buttonText: 'Добавить',
-      descriptionText: 'Пример: https://lorem-rss.hexlet.app/feed',
-      errorMessages: '',
-      urlInvalid: 'Ссылка должна быть валидным URL',
-      urlRequired: 'URL не должен быть пустым',
-      xmlError: 'Некорректный формат данных: не является XML/RSS',
-    },
-    post: {
-      title: 'Посты',
-      button: 'Просмотр',
-    },
-    feed: {
-      title: 'Фиды',
+    interfaceText: {
+      rssForm: {
+        title: 'RSS агрегатор',
+        subtitle: 'Начните читать RSS сегодня! Это легко, это красиво.',
+        inputText: 'Ссылка RSS',
+        buttonText: 'Добавить',
+        descriptionText: 'Пример: https://lorem-rss.hexlet.app/feed',
+      },
+      post: {
+        title: 'Посты',
+        button: 'Просмотр',
+      },
+      feed: {
+        title: 'Фиды',
+      },
+      errors: {
+        error: 'Ошибка: ',
+        networkError: 'Отсутствует ответ от сервера. Возможно, нет сети.',
+        errorMessages: '',
+        urlInvalid: 'Ссылка должна быть валидным URL',
+        urlRequired: 'URL не должен быть пустым',
+        xmlError: 'Некорректный формат данных: не является XML/RSS',
+      },
     },
   },
 };
