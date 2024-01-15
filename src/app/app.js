c3688a26884ba2fa81bd618e843db71c38905543
Slava Haikin <97975418+Vyachowski@users.noreply.github.com>
2024-01-15 19:13:08 +0200

    feat: add new high-level logic for app

--- src/app/app.js
+++ src/app/app.js
@@ -3,9 +3,10 @@ import { inputController, formController } from './appController.js';
 const app = (i18next) => {
   const formElement = document.querySelector('.rss-form');
   const [inputElement] = formElement.elements;
+  const { watchedState, setState } = createWatchedState(i18next);
 
-  inputElement.addEventListener('input', (e) => inputController(e, i18next));
-  formElement.addEventListener('submit', (e) => formController(e, i18next));
+  inputElement.addEventListener('input', (e) => inputController(e, watchedState, setState));
+  formElement.addEventListener('submit', (e) => formController(e, watchedState, setState));
 };
 
 export default app;
