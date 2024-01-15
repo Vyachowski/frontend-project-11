62eb375d18b6e386db0bdaff803c32b153dd2a9e
Slava Haikin <97975418+Vyachowski@users.noreply.github.com>
2024-01-16 01:25:52 +0200

    refactor: Update app.js

--- src/app/app.js
+++ src/app/app.js
@@ -2,9 +2,10 @@ import { inputController, formController } from './appController.js';
 import createWatchedState from './appModel.js';
 
 const app = (i18next) => {
+  const { watchedState, setState } = createWatchedState(i18next);
+
   const formElement = document.querySelector('.rss-form');
   const [inputElement] = formElement.elements;
-  const { watchedState, setState } = createWatchedState(i18next);
 
   inputElement.addEventListener('input', (e) => inputController(e, watchedState, setState));
   formElement.addEventListener('submit', (e) => formController(e, watchedState, setState));
