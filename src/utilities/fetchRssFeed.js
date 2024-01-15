7aedb319a7cbc020779d1674991acf2184565de8
Slava Haikin <97975418+Vyachowski@users.noreply.github.com>
2024-01-15 13:30:17 +0200

    refactor: apply linting rules

--- src/utilities/fetchRssFeed.js
+++ src/utilities/fetchRssFeed.js
@@ -1,24 +1,22 @@
-import axios from "axios";
+import axios from 'axios';
 
-const fetchRssFeed = (rssLink, i18next) => {
-  return axios
-    .get(rssLink)
-    .then((response) => {
-      const contentType = response.headers['content-headers'];
-      if (!contentType || !contentType.includes('application/xml')) {
-        throw new Error(i18next.t('rssForm.xmlError'));
-      }
-      return response.data;
-    })
-    .catch((error) => {
-      if (error.response) {
-        throw new Error(`${i18next.t('rssForm.error')}${error.response.status}`);
-      } else if (error.request) {
-        throw new Error(i18next.t('rssForm.networkError'));
-      } else {
-        throw new Error(`${i18next.t('rssForm.error')}${error.message}`);
-      }
-    });
-}
+const fetchRssFeed = (rssLink, i18next) => axios
+  .get(rssLink)
+  .then((response) => {
+    const contentType = response.headers['content-headers'];
+    if (!contentType || !contentType.includes('application/xml')) {
+      throw new Error(i18next.t('rssForm.xmlError'));
+    }
+    return response.data;
+  })
+  .catch((error) => {
+    if (error.response) {
+      throw new Error(`${i18next.t('rssForm.error')}${error.response.status}`);
+    } else if (error.request) {
+      throw new Error(i18next.t('rssForm.networkError'));
+    } else {
+      throw new Error(`${i18next.t('rssForm.error')}${error.message}`);
+    }
+  });
 
-export  default fetchRssFeed;
+export default fetchRssFeed;
