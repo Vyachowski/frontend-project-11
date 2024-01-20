9398803d07359a50dbd6cebeddd8f492be9932d8
Slava Haikin <97975418+Vyachowski@users.noreply.github.com>
2024-01-20 18:18:46 +0200

    fix: add null checker at rendering functions

--- src/app/render/renderers/renderFeed.js
+++ src/app/render/renderers/renderFeed.js
@@ -3,20 +3,24 @@ import renderListElement from './renderListElement.js';
 const renderPosts = ({ translation }, newPostsList, postsList) => {
   const { button: postButtonText } = translation.post;
   const postsListGroupElement = document.querySelector('.list-group-posts');
-  const newPostsNumbers = newPostsList.length - postsList.length;
-  const newPosts = newPostsList.slice(0, newPostsNumbers);
-  const renderedPostsList = renderListElement('posts', newPosts, postButtonText);
+  const newPostsNumber = newPostsList.length - postsList.length;
+  if (newPostsNumber) {
+    const newPosts = newPostsList.slice(0, newPostsNumber);
+    const renderedPostsList = renderListElement('posts', newPosts, postButtonText);
 
-  renderedPostsList.forEach((item) => postsListGroupElement.prepend(item));
+    renderedPostsList.forEach((item) => postsListGroupElement.prepend(item));
+  }
 };
 
 const renderFeeds = (newFeedsList, feedList) => {
   const feedsListGroupElement = document.querySelector('.list-group-feeds');
   const newFeedsNumber = newFeedsList.length - feedList.length;
-  const newFeeds = newFeedsList.slice(0, newFeedsNumber);
-  const renderedNewFeeds = renderListElement('feeds', newFeeds);
+  if (newFeedsNumber) {
+    const newFeeds = newFeedsList.slice(0, newFeedsNumber);
+    const renderedNewFeeds = renderListElement('feeds', newFeeds);
 
-  renderedNewFeeds.forEach((item) => feedsListGroupElement.prepend(item));
+    renderedNewFeeds.forEach((item) => feedsListGroupElement.prepend(item));
+  }
 };
 
 export { renderPosts, renderFeeds };
