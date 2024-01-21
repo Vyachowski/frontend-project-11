963eb72541bf1e337143a4c03f84729279bbd35e
Slava Haikin <97975418+Vyachowski@users.noreply.github.com>
2024-01-21 18:23:50 +0200

    feat: add modal window interface translation in render function

--- src/app/render/renderers/renderModalWindow.js
+++ src/app/render/renderers/renderModalWindow.js
@@ -8,10 +8,13 @@ const renderModalWindow = (watchedState, value) => {
   const modalTitle = document.querySelector('.modal-title');
   const modalText = document.querySelector('.modal-body');
   const modalLink = document.querySelector('.full-article');
+  const closeButton = document.querySelector('[data-i18n="modalWindow.secondaryButton"]');
   const { title, description, link } = watchedState.uiState.activePost;
 
   setElementText(modalTitle, title);
   setElementText(modalText, description);
+  setElementText(modalLink, watchedState.translation.modalWindow.mainButton);
+  setElementText(closeButton, watchedState.translation.modalWindow.secondaryButton);
   setElementAttributes(modalLink, { href: link });
 };
 
