bc1ffb5249964106145bd79ce716afe0edd1323c
Slava Haikin <97975418+Vyachowski@users.noreply.github.com>
2024-01-21 18:06:19 +0200

    feat: add modal window text

--- src/app/render/renderers/renderModalWindow.js
+++ src/app/render/renderers/renderModalWindow.js
@@ -1,11 +1,18 @@
-const renderModalWindow = (state, value) => {
-  const modalWindow = document.getElementById('modal');
+import setElementText from "../../../element_utilities/setElementText.js";
+import setElementAttributes from "../../../element_utilities/setElementAttributes.js";
 
+const renderModalWindow = (watchedState, value) => {
   if (!value) {
-    modalWindow.classList.remove('show');
-  } else {
-    modalWindow.classList.add('show');
+    return;
   }
+  const modalTitle = document.querySelector('.modal-title');
+  const modalText = document.querySelector('.modal-body');
+  const modalLink = document.querySelector('.full-article');
+  const { title, description, link } = watchedState.uiState.activePost;
+
+  setElementText(modalTitle, title);
+  setElementText(modalText, description);
+  setElementAttributes(modalLink, {href: link});
 };
 
 export default renderModalWindow;
