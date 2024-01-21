e4ae9a458895009d6fd20d45131c3fe710d8bc4f
Slava Haikin <97975418+Vyachowski@users.noreply.github.com>
2024-01-21 17:05:10 +0200

    refactor: rename controller import

--- src/app/controller/index.js
+++ src/app/controller/index.js
@@ -1,13 +1,13 @@
 import inputController from './controllers/inputController.js';
 import formController from './controllers/formController.js';
 import viewButtonController from './controllers/viewButtonController.js';
-import closeButtonController from './controllers/closeButtonController.js';
+import closeModalWindowButtonController from './controllers/modalWindowCloseButtonController.js';
 import rssUpdateController from './controllers/rssUpdateController.js';
 
 export {
   inputController,
   formController,
   viewButtonController,
-  closeButtonController,
+  closeModalWindowButtonController,
   rssUpdateController,
 };
