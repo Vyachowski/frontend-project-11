2864fb724a564da91492ae5b50c78ae9c10b6f46
Slava Haikin <97975418+Vyachowski@users.noreply.github.com>
2024-01-21 15:14:24 +0200

    refactor: delete excess console.log

--- src/app/controller/controllers/viewButtonController.js
+++ src/app/controller/controllers/viewButtonController.js
@@ -1,8 +1,8 @@
 const viewButtonController = (e, watchedState) => {
   const dataIdValue = e.target.getAttribute('data-id');
+
   if (!watchedState.uiState.viewedPosts.includes(dataIdValue)) {
     watchedState.uiState.viewedPosts.unshift(dataIdValue);
-    console.log(watchedState.uiState.viewedPosts)
   }
 };
 
