fe06d4d7817aff749932d2897aa93f1e36c56c0b
Slava Haikin <97975418+Vyachowski@users.noreply.github.com>
2024-01-26 14:32:58 +0200

    fix: remove unused code and import

--- src/index.js
+++ src/index.js
@@ -1,15 +1,10 @@
 import * as i18n from 'i18next';
-import { Modal } from 'bootstrap';
 import resources from './app/locales/index.js';
 import app from './app/app.js';
 import './styles.scss';
+import 'bootstrap';
 
 const runApp = () => {
-  document.querySelectorAll('[data-bs-toggle="modal"]')
-    .forEach((modal) => {
-      new Modal(modal);
-    });
-
   const i18nextInstance = i18n.createInstance();
   i18nextInstance.init({
     lng: 'ru',
