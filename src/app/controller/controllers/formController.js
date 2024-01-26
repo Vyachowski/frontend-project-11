977042ecd33efb2d9be6c6f4a6a9b3e939f1f4fb
Slava Haikin <97975418+Vyachowski@users.noreply.github.com>
2024-01-26 23:35:48 +0200

    refactor: replace excess import with self-invoking function

--- src/app/controller/controllers/formController.js
+++ src/app/controller/controllers/formController.js
@@ -1,15 +1,28 @@
+import { setLocale } from 'yup';
+import * as yup from 'yup';
 import parseXmlDocument from '../../../other_utilities/parseXmlDocument.js';
 import fetchRssFeed from '../../../other_utilities/fetchRssFeed.js';
 import getFeedAndPostsFromRssDocument from '../../../other_utilities/getFeedAndPostsFromRssDocument.js';
-import createUrlSchema from '../../../other_utilities/createUrlSchema.js';
 import createRssLinkWithProxy from '../../../other_utilities/createRssLinkWithProxy.js';
+
+const urlSchema = (() => {
+  setLocale({
+    mixed: {
+      required: 'urlRequired',
+    },
+    string: {
+      url: 'urlInvalid',
+    },
+  });
+
+  return yup.string().required().url();
+})();
 
 const formController = (e, watchedState, setFormState) => {
   e.preventDefault();
   setFormState(watchedState, 'sending');
 
   const errorInfo = { errorMessage: '', isUrlValid: false };
-  const urlSchema = createUrlSchema();
   const feedLink = watchedState.rssFormProcessing.rssUrl;
 
   if (watchedState.feedsUrls.includes(feedLink)) {
